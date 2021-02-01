5afc8fc6c46beff370b67917b33cd47cf3951415
unknown <tiago.plaza@pmovil.com.br>
2021-01-31 22:11:26 -0300

    assinando topic no firebase

--- App.js
+++ App.js
@@ -3,11 +3,33 @@ import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import messaging from '@react-native-firebase/messaging';
 
 import Rotas from './Views/Rotas';
 import Reducer from './Reducer';
 
 export default class App extends Component {
+  saveTokenToDatabase = async (token) => {
+    console.log("TOKEN = ", token)
+    //Salve o token no banco de dados do seu sistema, como salva qualquer outro conteúdo que você utiliza
+  }
+  componentDidMount(){
+    messaging()
+    .subscribeToTopic('blast')
+    .then(() => console.log('Subscribed to topic!'));
+      // Pega o token do dispositivo
+      messaging()
+        .getToken()
+        .then(token => {
+          return this.saveTokenToDatabase(token);
+        });
+        
+     
+      // escuta mudanças no token
+      return messaging().onTokenRefresh(token => {
+        this.saveTokenToDatabase(token);
+      });
+  }
   render(){
     return (
       <Provider store={createStore(Reducer, {}, applyMiddleware(ReduxThunk))}>
