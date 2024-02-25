d5f46e1a667325cbe0a58ba0771310f772df2f1c
Tiago Plaza <tiagoplaza@Tiagos-iMac-Pro.local>
2024-02-24 21:55:39 -0300

    new version RN 0.73.4

--- App.js
+++ App.js
@@ -1,49 +1,75 @@
-import React, {Component} from 'react';
-import { AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+import { PermissionsAndroid } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import messaging from '@react-native-firebase/messaging';
-import crashlytics from '@react-native-firebase/crashlytics';
 import FlashMessage from "react-native-flash-message";
 
-import Rotas from './Views/Rotas';
-import Reducer from './Reducer';
+import messaging from '@react-native-firebase/messaging';
 
+import Reducer from './redux';
+import Rotas from './screens/';
+
+const store = createStore(Reducer, {}, applyMiddleware(ReduxThunk));
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
 });
 
-export default class App extends Component {
+const requestNotificationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      {
+        title: 'Permissão de notificação do aplicativo Blast!',
+        message:
+          'Blast! precisa de permissão para receber notificações. ' +
+          'Você receberá as atualizações conforme sua escolha.',
+        buttonNeutral: 'Ver mais tarde',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the camera');
+    } else {
+      console.log('Camera permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+class App extends Component {
   saveTokenToDatabase = async (token) => {
     //console.log("TOKEN = ", token)
     AsyncStorage.setItem("TOKEN", token)
     //Salve o token no banco de dados do seu sistema, como salva qualquer outro conteúdo que você utiliza
   }
-  
+
   componentDidMount(){
-    crashlytics().log('Iniciando get Notification Geral.');
+    requestNotificationPermission();
     messaging()
     .subscribeToTopic('blast')
     .then(() => console.log('Subscribed to topic!'));
-      // Pega o token do dispositivo
-      messaging()
-        .getToken()
-        .then(token => {
-          return this.saveTokenToDatabase(token);
-        })
-        .catch(error => {
-          crashlytics().recordError(error);
-        });
-        
-     
-      // escuta mudanças no token
-      return messaging().onTokenRefresh(token => {
-        this.saveTokenToDatabase(token);
+
+    // Pega o token do dispositivo
+    messaging()
+      .getToken()
+      .then(token => {
+        return this.saveTokenToDatabase(token);
+      })
+      .catch(error => {
+        crashlytics().recordError(error);
       });
+      
+    
+    // escuta mudanças no token
+    return messaging().onTokenRefresh(token => {
+      this.saveTokenToDatabase(token);
+    });
   }
-  render(){
+  render() {
     return (
       <Provider store={createStore(Reducer, {}, applyMiddleware(ReduxThunk))}>
         <Rotas/>
@@ -51,6 +77,6 @@ export default class App extends Component {
       </Provider>
     );
   }
-};
+}
 
-AppRegistry.registerComponent(App, () => App);
+export default App;
